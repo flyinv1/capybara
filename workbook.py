35ce9db84ee9a883e9a1ec9d6b039019eef666fb
Matt Vredevoogd <matt.vredevoogd@gmail.com>
2021-12-04 20:39:05 -0800

    updated burnout data, chamber parameters

--- workbook.py
+++ workbook.py
@@ -7,11 +7,11 @@ import numpy as np
 from modules.units import qty
 
 # Initial parameters (Sutton)
-thrust = qty(350, "lbf").to("N")
+thrust = qty(500, "lbf").to("N")
 burn_time = qty(20, "s")
 Pc = qty(500, "psi")
 cstar = qty(1640, "m/s")
-isp = qty(260, "s")
+isp = qty(200, "s")
 g0 = qty(9.80665, "m/s^2")
 OF = 1.35
 
@@ -20,6 +20,8 @@ prop_mass = burn_time * mdot
 
 lox_mass = prop_mass.to("kg") / ( 1 + 1 / OF )
 ipa_mass = prop_mass.to("kg") / ( 1 + OF )
+
+print("mass flow: ", mdot.to("kg/s"))
 
 print("lox mass: ", lox_mass)
 print("ipa mass: ", ipa_mass)
@@ -30,8 +32,9 @@ ipa_rho = qty(786, "kg/m^3")
 lox_vol = lox_mass / lox_rho
 ipa_vol = ipa_mass / ipa_rho
 
-print("lox vol: ", lox_vol.to("in^3"))
-print("ipa vol: ", ipa_vol.to("in^3")) 
+print("lox vol: ", lox_vol.to("L"))
+print("ipa vol: ", ipa_vol.to("L")) 
+
 
 # %%
 
@@ -41,7 +44,7 @@ from modules.stress import fos, hoop, longitudinal
 
 P_tank = qty(900, "psi")
 thickness = qty(0.25, "in")
-r_outer = qty(2.5, "in")
+r_outer = qty(4, "in")
 r_inner = r_outer - 2 * thickness
 cap_thickness = qty(1, "in")
 al_6061T6_yield = qty(35000, "psi")
@@ -56,6 +59,7 @@ print("long fos: ", long_fos)
 min_lox_tank_length = lox_vol / (np.pi * r_inner ** 2) 
 min_ipa_tank_length = ipa_vol / (np.pi * r_inner ** 2)
 
+print("tank OD: ", (r_outer * 2).to("in"))
 print("lox tank length: ", min_lox_tank_length.to("in"))
 print("ipa tank length: ", min_ipa_tank_length.to("in"))
 
@@ -65,16 +69,70 @@ lox_tank_mass = ((np.pi * r_outer ** 2 - np.pi * r_inner ** 2) * min_lox_tank_le
 print("lox tank mass: ", lox_tank_mass.to("lb"))
 print("ipa tank mass: ", ipa_tank_mass.to("lb"))
 
+
 # %% 
 
 #### Altitude Requirements ####
 
-m0 = qty(90, "lb")
-mf = m0 - prop_mass
+ms = qty(40, "lb") + ipa_tank_mass + lox_tank_mass
+m0 = ms + prop_mass
+mr = m0 / ms
 
-dv = isp * g0 * np.log(m0 / mf) - g0 * burn_time
+print("mass ratio: ", mr)
+print("takoff T/W: ", (thrust / (g0 * m0)).to_base_units())
 
-print("dv: ", dv.to("m/s"))
+dv = isp * g0 * np.log(mr)
+
+cross_area = np.pi * r_outer ** 2
+Cd = qty(0.4, "dimensionless")
+rho_sea_air = qty(1.116, "kg/m^3")
+vavg = qty(450, "m/s")
+drag = 1 / 2 * Cd * rho_sea_air * (vavg / 2) ** 2 * cross_area
+
+v = lambda t: ((thrust - drag) / (m0 - mdot * t) - g0) * t
+z = lambda t: 1 / 2 * ((thrust - drag) / (m0 - mdot * t) - g0) * t ** 2
+
+vb = v(burn_time)
+zb = z(burn_time)
+zc = 1 / 2 * vb ** 2 / g0
+
+print("burnout:", zb.to("mi"))
+print("coast:", zc.to("mi"))
+print("max altitude:", (zb + zc).to("mi"))
+print("burnout velocity:", vb.to("mph"))
+print("drag:", drag.to("N"))
+
+# %%
+
+#### Engine Parameters ####
+
+Pa = qty(13.3, "psi")
+Pe = Pa # Optimize for 4000ft performance
+
+Pr = Pc / Pe
+AR = 5 # Area ratio for ideal expansion (k = 1.2)
+
+print("PR:", Pr)
+
+At = mdot * cstar / (Pc)
+Ae = At * AR
+Cf = thrust / (Pc * At)
+rt = np.sqrt(At / np.pi)
+re = np.sqrt(Ae / np.pi)
+dt = 2 * rt
+de = 2 * re
+
+Ac = 3 * At # Chamber Area
+rc = np.sqrt(Ac / np.pi)
+Lc = qty(6, "in")
+
+print("Cf:", Cf)
+print("Throat area:", At.to("in^2"))
+print("Throat diameter:", dt.to("in"))
+print("Exit area:", Ae.to("in^2"))
+print("Exit diameter:", de.to("in"))
+
+
 
 # %%
 
